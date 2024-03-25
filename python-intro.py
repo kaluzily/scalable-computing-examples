e8ed6b6dbd226e14ad85a112416f4fdb34febcaf
kaluzily <lmkaluzi@alaska.edu>
2024-03-25 15:40:57 -0700

    created files from Day 1 of dataworkshop

---
+++ python-intro.py
@@ -0,0 +1,22 @@
+x = 4
+
+print(x)
+
+str = 'Hello World'
+print(str)
+
+list = [100, 50, -20, 'text']
+print(list)
+list[1:3]
+
+list2 = ['more', 'things']
+list + list2
+
+tuple = ('a', 'b', 'c', 'd')
+tuple[0]
+tuple*3
+
+dict = {'name':['John', 'Doe'], 'age': [25, 30]}
+
+dict['name']
+dict.keys()
